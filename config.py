9c46ddb9586b12a57cb95ff23f2ab944d31485b8
Andu47 <andugolovatai@gmail.com>
2025-11-15 17:34:56 +0200

    config changes

--- config.py
+++ config.py
@@ -12,20 +12,20 @@ CORS_ALLOWED_ORIGINS = "*"
 
 # CAMERA USB (passthrough MJPEG)
 USB_DEVICE = "/dev/video0"      # sau "0" dacă vrei să folosești index și să-l convertești în cod
-USB_WIDTH = 640
-USB_HEIGHT = 480
+USB_WIDTH = 1024
+USB_HEIGHT = 576
 USB_FPS = 30
 USB_INPUT_FORMAT = "mjpeg"      # cerut ffmpeg / v4l2
 USB_LOG_EVERY_N_FRAMES = 60     # doar pentru debug
 
 # IP CAMERA (telefon)
-IP_CAMERA_URL = "http://10.146.163.97:8080/video"
-IP_BUFFERSIZE = 1               # ca să nu acumulezi lag
+IP_CAMERA_URL = "http://10.88.133.228:8080/video"
+IP_BUFFERSIZE = 1             # ca să nu acumulezi lag
 
 # QR WORKER
-QR_TARGET_FPS = 12              # cât de des procesezi cadrele
+QR_TARGET_FPS = 1            # cât de des procesezi cadrele
 QR_JPEG_QUALITY = 70
-QR_EMIT_INTERVAL = 1.0          # secunde: cât de des poți retrimite același text
+QR_EMIT_INTERVAL = 5.0        # secunde: cât de des poți retrimite același text
 
 # STREAM MJPEG (OUTPUT)
 STREAM_JPEG_QUALITY = 70        # pentru streamul QR cu overlay
@@ -55,10 +55,19 @@ ADDR_PCA=0x40
 FREQ_PCA=50
 MIN_PCA=500
 MAX_PCA=2500
+
 channelGrip=0 #gripperServo pe PCA
-channelPitch=1 #pitchServo pe PCA
 GRIP_CLOSED=180
 GRIP_OPEN=0
+
+channelPitch=1 #pitchServo pe PCA
+DEFAULT_PITCH=90
+FACT_PITCH=5
+MAX_PITCH=180
+MIN_PITCH=0
+
+#BMP280
+ADDR_BMP=0x76
 
 # DEBUG / LOGGING
 DEBUG = True
